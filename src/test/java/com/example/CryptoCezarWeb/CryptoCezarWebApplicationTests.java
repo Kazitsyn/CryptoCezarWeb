d77b0fb23a001907a4261d30d1954dab2da5244e
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2024-03-30 10:59:42 +0300

    Initial commit

---
+++ src/test/java/com/example/CryptoCezarWeb/CryptoCezarWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CryptoCezarWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCezarWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
