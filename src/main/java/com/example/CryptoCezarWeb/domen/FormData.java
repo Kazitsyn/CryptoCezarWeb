c3668a6b808dbdcd857d2b80cb47d71bfa6a5909
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2024-03-31 19:43:45 +0300

    form

---
+++ src/main/java/com/example/CryptoCezarWeb/domen/FormData.java
@@ -0,0 +1,12 @@
+package com.example.CryptoCezarWeb.domen;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class FormData {
+    private int pinCode;
+    private String word;
+}
